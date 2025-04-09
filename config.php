8364eecc08310f39679fb3104df5bed8a2c4ee8c
adinda syahila <adindasyahila04@gmail.com>
2025-04-09 12:55:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "guest";  // Ganti dari 'root' ke 'guest'
-$pass = "password";  // Masukkan password user 'guest'
+$pass = "password_guest";  // Masukkan password user 'guest'
 $db   = "hotel_reservasi";
 
 // Membuat koneksi
